4dc80f8fbb32f68ee15688c953bb92beb0460356
Smitha2704 <69708426+Smitha2704@users.noreply.github.com>
2020-12-28 14:12:51 +0530

    Add files via upload

---
+++ Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,58 @@
+//Create variables here
+var dog, happy_dog;
+var database, foodS;
+
+function preload()
+{
+  //load images here
+  dogImage = loadImage("images/dogImg.png");
+  happyDog = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  createCanvas(800, 700);
+  
+  database = firebase.database();
+
+  var foodRef = database.ref('foodS');
+  foodRef.on('value', readFoodStock);
+
+  
+
+  dog = createSprite(300,300,50,50);
+  dog.addImage(dogImage);
+  dog.scale = 0.4
+  
+}
+
+
+function draw() {  
+  background(46,139, 87);
+
+  fill("black");
+  text("Food Stock:" +foodS,200,50)
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDog);
+  }
+
+  drawSprites();
+  //add styles here
+
+}
+
+function readFoodStock(data){
+  foodS = data.val();
+}
+
+
+function writeStock(foodS){
+  if(foodS>0)
+    var foodNew = foodS-1;
+  else
+    var foodNew = foodS;
+  database.ref('/').update({
+    foodS:foodNew
+
+  })
+}
